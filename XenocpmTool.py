5dd6bd19c5312321395596cf0f167d4bb599073b
Tresehshs <162690468+Tresehshs@users.noreply.github.com>
2025-03-01 23:20:30 +0800

    Update XenocpmTool.py

--- XenocpmTool.py
+++ XenocpmTool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from tresehshs import Tresehshs
+from xenocpm import Tresehshs
 
 __CHANNEL_USERNAME__ = "XenoMain Channel"
 __GROUP_USERNAME__   = "XenoMain Chat"
