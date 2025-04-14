35559bce92164956136af2b6cf4b538541f58f53
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-15 01:02:31 +0800

    Update xenocpm.py

--- xenocpm.py
+++ xenocpm.py
@@ -1,6 +1,6 @@
 import requests
 
-__ENDPOINT_URL__: str = "https://mediumorchid-aardvark-464116.hostingersite.com/api"
+__ENDPOINT_URL__: str = "https://xeno1.squareweb.app/api"
 
 class Tresehshs:
     def __init__(self, access_key) -> None:
