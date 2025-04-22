f203e274ef64e08487afccebfd14e2eefbe9b030
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-22 18:16:46 +0800

    Update xenocpm.py

--- xenocpm.py
+++ xenocpm.py
@@ -251,8 +251,8 @@ class Tresehshs:
         "account_auth": self.auth_token,
         "car_id": car_id,
         "custom_mileage": custom_mileage,
-    }
-    params = { "key": self.access_key }
-    response = requests.post(f"{__ENDPOINT_URL__}/millage_car", params=params, data=payload)
-    response_decoded = response.json()
-    return response_decoded.get("ok")
+        }
+        params = { "key": self.access_key }
+        response = requests.post(f"{__ENDPOINT_URL__}/millage_car", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
