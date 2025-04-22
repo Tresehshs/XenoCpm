baf631450626bd1bea17f8cc72a1013e60258b36
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-22 19:14:49 +0800

    Update XenocpmTool.py

--- XenocpmTool.py
+++ XenocpmTool.py
@@ -669,7 +669,7 @@ if __name__ == "__main__":
                     console.print("[bold yellow][!] Please try again with valid values[/bold yellow]")
                     sleep(2)
                     continue
-                else:
+            else:
                 continue
             break
 
