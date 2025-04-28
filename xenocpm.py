5a3801e10ec78c257e6a75daa9ae58cdee0cde2c
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-28 09:37:57 +0800

    Update xenocpm.py

--- xenocpm.py
+++ xenocpm.py
@@ -284,5 +284,15 @@ class Tresehshs:
         response = requests.post(f"{__ENDPOINT_URL__}/rear_bumper", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
+        
+    def front_bumper(self, car_id):
+        payload = {
+        "account_auth": self.auth_token,
+        "car_id": car_id,
+        }
+        params = {"key": self.access_key}
+        response = requests.post(f"{__ENDPOINT_URL__}/front_bumper", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
  
 
