772c1fca8662e33d86bc89d701a0e41fc51e5cf7
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-28 12:34:18 +0800

    Update XenocpmTool.py

--- XenocpmTool.py
+++ XenocpmTool.py
@@ -98,7 +98,7 @@ def load_key_data(cpm):
     
     print(Colorate.Horizontal(Colors.red_to_yellow, '========[ ACCESS KEY DETAILS ]========'))
     
-    print(Colorate.Horizontal(Colors.red_to_yellow, f'Access Key : {data.get('access_key")}.'))
+    print(Colorate.Horizontal(Colors.black, f'Access Key : {data.get("access_key")}.'))
     
     print(Colorate.Horizontal(Colors.red_to_yellow, f'Telegram ID : {data.get("telegram_id")}.'))
     
