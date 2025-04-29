77b55043063e7dbf019f097264f7a3e5761dd9ff
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-04-29 20:13:36 +0800

    Update XenocpmTool.py

--- XenocpmTool.py
+++ XenocpmTool.py
@@ -86,7 +86,7 @@ def load_player_data(cpm):
             
         else:
             print(Colorate.Horizontal(Colors.red_to_yellow, '! ERROR: new accounts most be signed-in to the game at least once !.'))
-            exit(1)
+            sleep(1)
     else:
         print(Colorate.Horizontal(Colors.red_to_yellow, '! ERROR: seems like your login is not properly set !.'))
         exit(1)
