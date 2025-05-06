3d66d13b4a87e40133783434a3b1c8890e91d8d8
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-05-06 14:13:04 +0800

    Update xenocpm.py

--- xenocpm.py
+++ xenocpm.py
@@ -437,5 +437,12 @@ class Tresehshs:
         response = requests.post(f"{__ENDPOINT_URL__}/incline", params=params, data=payload)
         response_decoded = response.json()
         return response_decoded.get("ok")
+        
+    def unlock_all_lamborghinis(self) -> bool:
+        payload = { "account_auth": self.auth_token }
+        params = { "key": self.access_key }
+        response = requests.post(f"{__ENDPOINT_URL__}/unlock_all_lamborghinis", params=params, data=payload)
+        response_decoded = response.json()
+        return response_decoded.get("ok")
  
 
